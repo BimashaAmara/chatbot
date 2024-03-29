8945da4baeb25914a9d1b62378fa5c6588efa803
Bimasha Amarasinghe <120245310+BimashaAmara@users.noreply.github.com>
2024-03-29 14:52:31 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,88 +1,3 @@
-# import random
-# import os
-# from flask import Flask, render_template, request, jsonify
-# import numpy as np
-# import json
-# import pickle
-# import nltk
-# from nltk.stem import WordNetLemmatizer
-# from keras.models import load_model
-
-# lemmatizer = WordNetLemmatizer()
-
-# app = Flask(__name__, static_folder='templates')
-
-# ABSOLUTE_PATH = os.path.abspath(os.path.dirname(__file__))
-
-# def get_chatbot_response(user_input):
-#     intent_index = predict_user_input(user_input)
-#     intent = classes[intent_index]
-#     return get_response(intent)
-
-# with open(os.path.join(ABSOLUTE_PATH, 'words.pkl'), 'rb') as f:
-#     words = pickle.load(f)
-# with open(os.path.join(ABSOLUTE_PATH, 'classes.pkl'), 'rb') as f:
-#     classes = pickle.load(f)
-# model = load_model(os.path.join(ABSOLUTE_PATH, 'chatbot_model.keras'))
-
-# def get_response(intent):
-#     with open(os.path.join(ABSOLUTE_PATH, 'intents.json'), 'r') as file:
-#         intents_json = json.load(file)
-#     for intent_data in intents_json['intents']:
-#         if intent_data['tag'] == intent:
-#             response = random.choice(intent_data['responses'])
-#             return response
-
-# def preprocess_input(user_input):
-#     user_input = user_input.lower()
-#     user_input = nltk.word_tokenize(user_input)
-#     user_input = [lemmatizer.lemmatize(word) for word in user_input]
-#     return user_input
-
-# def predict_user_input(user_input):
-#     user_input = preprocess_input(user_input)
-#     user_input_bow = bow(user_input, words)
-#     user_input_bow = np.array(user_input_bow).reshape(1, -1)
-#     output = model.predict(user_input_bow)
-#     intent_index = np.argmax(output)
-#     return classes[intent_index]
-
-# def bow(sentence, words):
-#     sentence_words = clean_up_sentence(sentence)
-#     bag = [0] * len(words)
-#     for s in sentence_words:
-#         for i, w in enumerate(words):
-#             if w == s:
-#                 bag[i] = 1
-#     return bag
-
-# def clean_up_sentence(sentence):
-#     sentence_words = nltk.word_tokenize(sentence)
-#     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
-#     return sentence_words
-
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     if request.method == 'POST':
-#         user_input = request.json['user_input']
-#         response = get_chatbot_response(user_input)
-#         return jsonify({'user_input': user_input, 'response': response})
-#     return render_template('chatbot.html')
-
-# @app.errorhandler(500)
-# def internal_error(error):
-#     return str(error), 500
-
-# # @app.route('/chatbot', methods=['POST'])
-# # def chatbot():
-# #     user_input = request.json['user_input']
-# #     response = get_chatbot_response(user_input)
-# #     return jsonify({'response': response[0]})
-
-# if __name__ == '__main__':
-#     app.run(debug=True)  
-
-
 import random
 import os
 from flask import Flask, render_template, request, jsonify
@@ -106,24 +21,32 @@ with open(os.path.join(ABSOLUTE_PATH, 'classes.pkl'), 'rb') as f:
 model = load_model(os.path.join(ABSOLUTE_PATH, 'chatbot_model.keras'))
 
 def get_chatbot_response(user_input):
-    intent_index = predict_user_input(user_input)
-    if intent_index is None:
-        return "Sorry, I couldn't process your request."
-    intent = classes[intent_index]
-    return get_response(intent)
+    try:
+        intent_index = predict_user_input(user_input)
+        if intent_index is None:
+            return "Sorry, I couldn't process your request."
+        intent = classes[intent_index]
+        return get_response(intent)  # return the string response
+    except Exception as e:
+        print(f"Error getting chatbot response: {e}")
+        return "Sorry, an error occurred while processing your request."
 
 def get_response(intent):
     with open(os.path.join(ABSOLUTE_PATH, 'intents.json'), 'r') as file:
         intents_json = json.load(file)
     for intent_data in intents_json['intents']:
         if intent_data['tag'] == intent:
-            response = random.choice(intent_data['responses'])
-            return response
+            responses = intent_data['responses']
+            if isinstance(responses, list):
+                response = random.choice(responses)
+            else:
+                response = responses
+            return response  # return a string response
 
 def preprocess_input(user_input):
     user_input = user_input.lower()
     user_input = nltk.word_tokenize(user_input)
-    user_input = [lemmatizer.lemmatize(word) for word in user_input]
+    user_input = ' '.join([lemmatizer.lemmatize(word) for word in user_input])
     return user_input
 
 def predict_user_input(user_input):
@@ -133,9 +56,9 @@ def predict_user_input(user_input):
         user_input_bow = np.array(user_input_bow).reshape(1, -1)
         output = model.predict(user_input_bow)
         intent_index = np.argmax(output)
-        return classes[intent_index]
+        return int(intent_index)  
     except Exception as e:
-        print(f"Error predicting user input: {e}")
+        print(f"Error predicting user_input: {e}")
         return None
 
 def bow(sentence, words):
@@ -160,11 +83,9 @@ def index():
         return jsonify({'user_input': user_input, 'response': response})
     return render_template('chatbot.html')
 
-@app.route('/get_response', methods=['POST'])
+@app.route('/get_response', methods=['GET'])
 def get_response_route():
-    user_input = request.json['user_input']
-    response = get_chatbot_response(user_input)
-    return jsonify({'response': response})
+    return jsonify({'response': 'This is a static test response from /get_response endpoint.'})
 
 @app.errorhandler(500)
 def internal_error(error):
@@ -173,8 +94,11 @@ def internal_error(error):
 @app.route('/chatbot', methods=['POST'])
 def chatbot():
     user_input = request.json['user_input']
+    print(f"User input: {user_input}")
     response = get_chatbot_response(user_input)
-    return jsonify({'response': response})  
+    print(f"Response type: {type(response)}")
+    print(f"Response value: {response}")
+    return jsonify({'response': response})  # return the string response
 
 if __name__ == '__main__':
     app.run(debug=True)
